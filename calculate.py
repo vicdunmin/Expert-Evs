de3241bbba2e07de8e8a6054b0a1f61e70538db6
vicdunmin <zhudunmin@gmail.com>
2020-07-22 22:56:21 +0800

    Done with parsing

--- calculate.py
+++ calculate.py
@@ -2,24 +2,30 @@ import math
 import json
 
 with open('data/nature.json', 'r') as fp:
-    nature = json.load(fp)
+    nature_dict = json.load(fp)
 
 
 class stat(object):
 
-    def __init__(self, poke_dict, HP_IV=31, Def_IV=31, Spd_IV=31, disposable=510, level=100, phys_weight=0.4, no_nature=False):
+    def __init__(self, poke_dict, HP_IV=31, Def_IV=31, Spd_IV=31,
+                 disposable=510, level=100, phys_weight=0.5, allow_nature=True,
+                 phys_mod=1.0, spec_mod=1.0, method='c'):
         self.HP = poke_dict['HP']
         self.Def = poke_dict['Def']
         self.Spd = poke_dict['Spd']
+        self.Atk = poke_dict['Atk']
+        self.Spa = poke_dict['Spa']
         self.HP_IV = HP_IV
         self.Def_IV = Def_IV
         self.Spd_IV = Spd_IV
         self.disposable = disposable
         self.level = level
-        self.nature_dict = nature
+        self.nature_dict = nature_dict
         self.phys_weight = phys_weight
         self.spa_weight = 1.0 - phys_weight
-        self.no_nature = no_nature
+        self.phys_mod = phys_mod
+        self.spec_mod = spec_mod
+        self.allow_nature = allow_nature
 
     def hp_transform(self, ev):
         hp_stat = ((2 * self.HP + self.HP_IV + ev // 4)
@@ -36,31 +42,56 @@ class stat(object):
                        * self.level) // 100 + 5) * nature_mod
         return math.floor(other_stat)
 
+    def valid_evs(self, hp_ev, def_ev, spd_ev):
+        if hp_ev > 252 or def_ev > 252 or spd_ev > 252:
+            return False
+        if sum([hp_ev, def_ev, spd_ev]) > self.disposable:
+            return False
+        return True
+
     def calc(self):
         max_score = 0
-        for hp_ev in range(0, 255, 4):
-            for def_ev in range(0, 255, 4):
-                sum_ev = hp_ev + def_ev
-                if sum_ev < 255:
-                    continue
-                else:
-                    for spd_ev in range(0, self.disposable - sum_ev + 1, 4):
+        for hp_ev in range(0, self.disposable + 1, 4):
+            for def_ev in range(0, self.disposable + 1, 4):
+                for spd_ev in range(0, self.disposable + 1, 4):
+                    if self.valid_evs(hp_ev, def_ev, spd_ev):
                         for nat in self.nature_dict:
                             if "Def" in self.nature_dict[nat]:
-                                def_mod = float(self.nature_dict[nat]["Def"])
+                                def_mod = float(
+                                    self.nature_dict[nat]["Def"])
                                 spd_mod = 1.0
                             elif "Spd" in self.nature_dict[nat]:
-                                spd_mod = float(self.nature_dict[nat]["Spd"])
+                                spd_mod = float(
+                                    self.nature_dict[nat]["Spd"])
                                 def_mod = 1.0
-                            else:
+                            if not self.allow_nature:
                                 def_mod = spd_mod = 1.0
                             hp_stat = self.hp_transform(hp_ev)
-                            def_stat = self.def_transform(def_ev, def_mod)
-                            spd_stat = self.spd_transform(spd_ev, spd_mod)
+                            def_stat = self.def_transform(
+                                def_ev, def_mod) * self.phys_mod
+                            spd_stat = self.spd_transform(
+                                spd_ev, spd_mod) * self.spec_mod
                             score = hp_stat / \
                                 (self.phys_weight / def_stat +
                                  self.spa_weight / spd_stat)
                             if (score > max_score):
                                 max_score = score
-                                max_comb = [hp_ev, def_ev, spd_ev, nat]
+                                if not self.allow_nature:
+                                    max_comb = [hp_ev, def_ev,
+                                                spd_ev, 'no nature']
+                                else:
+                                    if nat == 'Bold' or nat == 'Impish':
+                                        nat = '+Def'
+                                        if int(self.Atk) >= int(self.Spa):
+                                            rec_nat = 'Impish'
+                                        else:
+                                            rec_nat = 'Bold'
+                                    else:
+                                        nat = '+Spd'
+                                        if (int(self.Atk) >= int(self.Spa)):
+                                            rec_nat = 'Careful'
+                                        else:
+                                            rec_nat = 'Calm'
+                                    max_comb = [hp_ev, def_ev,
+                                                spd_ev, nat, rec_nat]
         return max_comb
--- main.py
+++ main.py
@@ -1,16 +1,51 @@
 from get_stat import get_info
 from calculate import stat
 import json
+import argparse
 
 
 if __name__ == '__main__':
-    poke_name = input(
-        'Please enter in the pokemon name you want to calculate: ').lower()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--pokemon',
+                        help='Select the pokemon you want to EV')
+    parser.add_argument('-d', '--disposable',
+                        help='The EVs to be put into bulk calculation', default=510)
+    parser.add_argument(
+        '-n', '--nature', help='whether or not to allow nature modifier', default=True)
+    parser.add_argument('-pw', '--phys_weight',
+                        help='physical weight of the bulk of the pokemon', default=0.5)
+    parser.add_argument('-pm', '--phys_modifier',
+                        help='physical phys_modifier of the bulk of the pokemon', default=1.0)
+    parser.add_argument('-sm', '--spec_modifier',
+                        help='special phys_modifier of the bulk of the pokemon', default=1.0)
+    parser.add_argument(
+        '-m', '--method', help='either the chinese method or smogon method', default='c')
+    args = parser.parse_args()
+
+    poke_name = args.pokemon.lower()
+    disposable_ev = int(args.disposable)
+    if args.nature == 'false' or args.nature == 'False':
+        nature = False
+    else:
+        nature = True
+    phys_weight = float(args.phys_weight)
+    phys_modifier = float(args.phys_modifier)
+    spec_modifier = float(args.spec_modifier)
+    method = args.method
+
     with open('data/poke_stat.json', 'r') as fp:
         poke_stat = json.load(fp)
     if poke_name not in poke_stat:
         poke_stat = get_info(poke_name, poke_stat)
         with open('data/poke_stat.json', 'w') as fp:
             json.dump(poke_stat, fp)
-    p_stat = stat(poke_stat[poke_name])
-    print(p_stat.calc())
+    p_stat = stat(poke_stat[poke_name],
+                  disposable=disposable_ev, phys_weight=phys_weight,
+                  phys_mod=phys_modifier, spec_mod=spec_modifier,
+                  allow_nature=nature,
+                  method=method)
+    hp_ev, def_ev, spd_ev, nat, rec_nat = p_stat.calc()
+    print('Pokemon name: {}, Disposable Total EVs: {}'.format(
+        poke_name, disposable_ev))
+    print('EVs: {} HP / {} Def / {} Spd'.format(hp_ev, def_ev, spd_ev))
+    print('Nature modifier: {}, Recommended nature: {}'.format(nat, rec_nat))
